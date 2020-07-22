4322c1b85c2bef3071edf723a7b985c8598b28ba
Palash Mondal <palashm@live.in>
2020-07-22 11:18:35 +0530

    Create a custom service

--- api/product/services/product.js
+++ api/product/services/product.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Promise to fetch all records
+   *
+   * @return {Promise}
+   */
+  find(params, populate) {
+    return strapi
+      .query("product")
+      .model.find(params, populate)
+      .select("title price qty");
+  },
+};
